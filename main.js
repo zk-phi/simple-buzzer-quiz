761ae1b48fe34ac483dcd4fb0ec03aefa577f915
zk-phi <zk-phi@users.noreply.github.com>
2025-11-04 20:25:51 +0900

    Revert "Add cache-control headers"
    This reverts commit b7a6250eed27930e2bd1f704031ed35017fdbb36.

--- main.js
+++ main.js
@@ -96,8 +96,6 @@ const vm = new Vue({
       };
       xhr.onerror = function () { vm.loadError = true; };
       xhr.open("GET", match ? `https://${match[1]}` : "problems.json", true);
-      xhr.setRequestHeader("Pragma", "no-cache");
-      xhr.setRequestHeader("Cache-Control", "no-cache");
       xhr.send(null);
     },
     monitorLoadingStatus: function () {
