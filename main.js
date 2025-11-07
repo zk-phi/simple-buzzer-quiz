b09c7534a5d7c26e4443ba5fdae3332c8c0ae94c
zk-phi <zk-phi@users.noreply.github.com>
2025-11-08 02:11:58 +0900

    Rename function

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ const vm = new Vue({
       const xhr = new XMLHttpRequest();
       xhr.onload = function () {
         if (xhr.responseText.startsWith("作問テンプレートv1.0")) {
-          vm.problems = importTsv1(xhr.responseText);
+          vm.problems = importTsv1_0(xhr.responseText);
         } else {
           vm.problems = JSON.parse(xhr.responseText);
         }
