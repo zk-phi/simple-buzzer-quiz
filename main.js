1517bb4ed37f184d7535f1c4757a87b76ac1f548
sickleaf <5824061+sickleaf@users.noreply.github.com>
2025-11-24 16:38:34 +0900

    added publiQa tag in Tweet text

--- main.js
+++ main.js
@@ -76,11 +76,11 @@ const vm = new Vue({
   filters: {
     toShareUrlCorrect: function (problem) {
       return "https://twitter.com/intent/tweet?text=" +
-             "「" + problem + "」に正解した！" + location.href;
+             "「" + problem + "」に正解した！" + location.href + " #publiQa";
     },
     toShareUrlWrong: function (problem) {
       return "https://twitter.com/intent/tweet?text=" +
-             "「" + problem + "」に正解できなかった😭" + location.href;
+             "「" + problem + "」に正解できなかった😭" + location.href + " #publiQa";
     },
   },
   methods: {
