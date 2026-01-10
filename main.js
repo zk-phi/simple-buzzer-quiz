73bed89051dbe805b87346bad0d74584517b6a21
zk-phi <zk-phi@users.noreply.github.com>
2026-01-10 10:34:27 +0900

    Revert "Refactoring"
    This reverts commit 8a15037fec31c74dfaa4e8cdadfbb51ff1e7e883.

--- main.js
+++ main.js
@@ -4,17 +4,15 @@ const BS_PENALTY    = 200; /* msec */
 const PIE_DASHARRAY = 63;
 const IS_TOUCH = "ontouchstart" in window;
 
-/* DESTRUCTIVE: Shuffle an array. */
-const shuffleArray = (array) => {
+function shuffleArray (array) {
   for (var i = array.length - 1; i > 0; i--) {
     var r = Math.floor(Math.random() * (i + 1));
     var tmp = array[i];
     array[i] = array[r];
     array[r] = tmp;
   }
-};
+}
 
-/* () => Promise<Problems> */
 const loadProblems = () => (
   new Promise((resolve, reject) => {
     const match = location.href.match(/\?(.+)$/);
@@ -193,6 +191,9 @@ const vm = new Vue({
     },
     stopProblem: function () {
       this.playAudio(SOUNDS.ANSWER);
+      this.startInput();
+    },
+    startInput: function () {
       this.playAudio(SOUNDS.TIMER);
       this.inputTimer = INPUT_TIMER;
       this.alphaInput = "";
